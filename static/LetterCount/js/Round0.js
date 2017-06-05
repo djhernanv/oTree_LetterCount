2908b21a46f37372e9b8bc15f3697c473fb5a92b
georgschmidt <georg.schmidt@hotmail.com>
2017-06-05 16:36:33 +0200

    Update Round0.js

--- static/LetterCount/js/Round0.js
+++ static/LetterCount/js/Round0.js
@@ -1,5 +1,5 @@
-var sequences_0 = ["atdpsiwrar","dyshafudak","yavaksvnsm","vhgqgqakwf","dmjlazjena","aimrodkuap","rssgragaza","dakhzwxpgo","tnrabmcfob","rtqonabnva","laaaaxzpcx","fawykauoyp","wxsrcqaaqa","abankaqaeb","dptxbeivua","iranbgtadl","uxcyvabfro","hjanyeqphq","nierfauala","paqzfwdran","alsiaauhkh","patpaizgnj","allsfdhogg","apanslapij","afvaepchfp","htuaqaatai","vaxvyhafpa","vqaobwyfan","wtjaaekysg","qaaghrzbhu"];
-var solutions_0 = [2,2,2,1,2,2,3,1,1,2,4,2,3,4,1,2,1,1,3,2,3,2,1,3,2,4,3,2,2,2];
+var sequences_0 = ["wgaaojlbenndnlfajaqqyv","wyafzlgacvagidwjzzayaharrwgxaxnhelvaoxizrlfsgafcoalxybaamnbfgxcptwopadajajnagkazzagzalamuj","hvkpjipslkuo","yaaqxjjhazaptaknryqkimguquqnerodxlqajizaybbadjvkarhc","awgfxamulbeayoaafvyieabkwldpuiadrk"];
+var solutions_0 = [4,20,0,9,7,];
 var length_0 = solutions_0.length;
 
 var me = me || {};
